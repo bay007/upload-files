234c9c616d138be757a0ac11e01f27ccf3438218
bay007 <tnt.galicia@gmail.com>
2017-04-13 03:12:58 -0500

    FO: Agregando menus contextuales de acciones varias.
    Cada imagen se agrega un menu donde se puede eliminar una imagen, hacerla favorita, quitar de favoritos, las acciones de los menus son dummy

--- src/PhotoEditor/PhotoEditor.js
+++ src/PhotoEditor/PhotoEditor.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { Component } from 'react';
+import { Alert, Intent, IconClasses, Button } from '@blueprintjs/core';
+import {
+  Classes,
+  ITetherConstraint,
+  Menu,
+  MenuDivider,
+  MenuItem,
+  Popover,
+  PopoverInteractionKind,
+  Position,
+} from '@blueprintjs/core';
 
 const PhotoEditor = ({ photos }) => {
   const items = photos.map(photo => {
     return (
       <div key={photo.id}>
-
         <PhotoItem photo={photo} />
       </div>
     );
@@ -19,22 +29,135 @@ const PhotoEditor = ({ photos }) => {
 
 export default PhotoEditor;
 
-function ImageOptions({ id }) {
-  function onclickUploadPhoto(a) {
-    alert(a);
+class ImageOptions extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpenMenu: false,
+      isOpenAlertDelete: false,
+    };
+
+    this.onclickToggleMenu = this.onclickToggleMenu.bind(this);
+    this.handleMoveClose = this.handleMoveClose.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
 
-  return (
-    <div onClick={onclickUploadPhoto.bind(this, id)}>
-      Image Options
-    </div>
-  );
+  onclickToggleMenu(a) {
+    this.setState({ isOpenMenu: !this.state.isOpenMenu });
+  }
+
+  handleMoveClose() {
+    alert('Enviar ACTION a mobx para modificar el estado');
+    this.setState({ isOpenAlertDelete: false });
+  }
+
+  handleClose() {
+    this.setState({ isOpenAlertDelete: false });
+  }
+
+  render() {
+    const isEditableIcon = this.props.photo.isEditable
+      ? IconClasses.STAR
+      : IconClasses.STAR_EMPTY;
+
+    const isEditableColor = this.props.photo.isEditable
+      ? 'pt-intent-primary'
+      : null;
+
+    const menu = () => {
+      return (
+        <Menu>
+          <MenuItem
+            iconName={IconClasses.STAR}
+            text="Hacer editable"
+            disabled={this.props.photo.isEditable}
+            onClick={() => {
+              alert('mandar ACTION a mobx modificar isEditable flag');
+              this.setState({ isOpenMenu: !this.state.isOpenMenu });
+            }}
+          />
+          <MenuItem
+            iconName={IconClasses.STAR_EMPTY}
+            text="Quitar editable"
+            disabled={!this.props.photo.isEditable}
+            onClick={() => {
+              alert('mandar ACTION a mobx modificar isEditable flag');
+              this.setState({ isOpenMenu: !this.state.isOpenMenu });
+            }}
+          />
+          <MenuDivider title="Editar" />
+          <MenuItem
+            iconName={IconClasses.EDIT}
+            text="Editar Menu-Items"
+            disabled={!this.props.photo.isEditable}
+            onClick={() => {
+              alert('Link a Modify Menu Items');
+              this.setState({ isOpenMenu: !this.state.isOpenMenu });
+            }}
+          />
+          <MenuDivider title="Cuidado" />
+          <MenuItem
+            iconName={`${IconClasses.TRASH} pt-intent-danger`}
+            text="¡Eliminar!"
+            onClick={() => {
+              this.setState({ isOpenAlertDelete: true });
+              this.setState({ isOpenMenu: !this.state.isOpenMenu });
+            }}
+          />
+        </Menu>
+      );
+    };
+
+    const alert = () => {
+      return (
+        <Alert
+          intent={Intent.PRIMARY}
+          isOpen={this.state.isOpenAlertDelete}
+          confirmButtonText="¿Eliminar?"
+          cancelButtonText="Cancelar"
+          onConfirm={this.handleMoveClose}
+          onCancel={this.handleClose}
+        >
+          <p>
+            ¿Realmente quiere
+            {' '}
+            <b>Eliminar?</b>
+            {' '}
+            {this.props.photo.isEditable
+              ? 'Si elimina todos sus avances se perderán'
+              : null}
+          </p>
+        </Alert>
+      );
+    };
+
+    return (
+      <div className="pt-button-group pt-large pt-fill">
+        <Button iconName={isEditableIcon} className={isEditableColor} />
+
+        <Popover
+          content={menu()}
+          interactionKind={PopoverInteractionKind.CLICK}
+          isOpenMenu={this.state.isOpenMenu}
+          position={Position.RIGHT}
+          useSmartPositioning={true}
+        >
+          <Button
+            iconName={`${IconClasses.MENU} `}
+            onClick={this.onclickToggleMenu}
+          />
+
+        </Popover>
+        {alert()}
+      </div>
+    );
+  }
 }
 
 function PhotoItem({ photo }) {
   return (
-    <div>
-      <ImageOptions id={photo.id} />
+    <div className="pt-card pt-elevation-3">
+      <ImageOptions photo={photo} />
       <img src={photo.url} />
     </div>
   );
@@ -46,7 +169,10 @@ function UploadPhoto() {
   }
 
   return (
-    <div className="uploadPhoto" onClick={onclickUploadPhoto}>
+    <div
+      className="uploadPhoto pt-card pt-elevation-3"
+      onClick={onclickUploadPhoto}
+    >
       <input type="file" />
       Seleccione aqui su archivo
     </div>
